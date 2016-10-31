0c941b7656d03e102a89b0a18a6ba11f8585558c
UnAfraid <unafraid89@gmail.com>
2016-11-01 00:53:05 +0200

    Fist guy who write to the bot is considered master

--- src/main/java/com/github/unafraid/spring/bot/handlers/impl/StartHandler.java
+++ src/main/java/com/github/unafraid/spring/bot/handlers/impl/StartHandler.java
@@ -1,11 +1,13 @@
 package com.github.unafraid.spring.bot.handlers.impl;
 
+import com.github.unafraid.spring.bot.db.services.IUsersService;
 import com.github.unafraid.spring.bot.util.BotUtil;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import javax.inject.Inject;
 import java.util.List;
 
 /**
@@ -13,6 +15,9 @@ import java.util.List;
  */
 @Service
 public final class StartHandler implements ICommandHandler {
+    @Inject
+    private IUsersService usersService;
+
     @Override
     public String getCommand() {
         return "/start";
@@ -30,6 +35,11 @@ public final class StartHandler implements ICommandHandler {
 
     @Override
     public void onMessage(AbsSender bot, Message message, int updateId, List<String> args) throws TelegramApiException {
-        BotUtil.sendMessage(bot, message, "Hello, i am " + bot.getMe().getUserName() + ", if you want to know what i can do type /start", true, false, null);
+        if (usersService.findAll().isEmpty()) {
+            usersService.create(message.getFrom().getId(), message.getFrom().getUserName(), 10);
+            BotUtil.sendMessage(bot, message, "Hello master, i am " + bot.getMe().getUserName() + ", if you want to know what i can do type /start", true, false, null);
+        } else {
+            BotUtil.sendMessage(bot, message, "Hello, i am " + bot.getMe().getUserName() + ", if you want to know what i can do type /start", true, false, null);
+        }
     }
 }
