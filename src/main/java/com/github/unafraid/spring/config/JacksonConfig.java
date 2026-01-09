e990018af6f447d5fea961dbd24b254ba13583a7
UnAfraid <unafraid89@gmail.com>
2026-01-09 13:48:18 +0200

    Auto discover object mapper modules

--- src/main/java/com/github/unafraid/spring/config/JacksonConfig.java
+++ src/main/java/com/github/unafraid/spring/config/JacksonConfig.java
@@ -1,7 +1,6 @@
 package com.github.unafraid.spring.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +9,6 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        return mapper;
+        return mapper.findAndRegisterModules();
     }
 }
